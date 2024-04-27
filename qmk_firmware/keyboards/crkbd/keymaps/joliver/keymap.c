1bf02a3f5b203d872fc87042b1330274810e525e
joliv3r <jacobbruun@hotmail.no>
2024-04-27 23:06:24 +0200

    Changed order of layers

--- qmk_firmware/keyboards/crkbd/keymaps/joliver/keymap.c
+++ qmk_firmware/keyboards/crkbd/keymaps/joliver/keymap.c
@@ -19,11 +19,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layers {
     _BASE,
+    _CLMK,
+    _NUM,
     _SYM,
     _NAV,
-    _NUM,
-    _FN,
-    _CLMK
+    _FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,6 +38,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             LGUI_T(KC_DEL), LT(_NUM, KC_BSPC), LT(_NAV, KC_ENT),           LT(_FN, KC_TAB), LT(_SYM, KC_SPC),  RALT_T(KC_ESC)
   ),
 
+    [_CLMK] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                             ,-----------------------------------------------------.
+      XXXXXXX,         KC_Q,         KC_W,       KC_F,           KC_P,    KC_B,          KC_J,         KC_L,         KC_U,         KC_Y,           KC_SCLN,  KC_MINS,
+  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),    KC_G,          KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,         KC_Z,         KC_X,       KC_C,           KC_D,    KC_B,          KC_K,         KC_H,      KC_COMM,       KC_DOT,         KC_SLSH,  KC_ESC,
+  //|--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
+                            LGUI_T(KC_DEL), LT(_NUM, KC_BSPC), LT(_NAV, KC_ENT),           LT(_FN, KC_TAB), LT(_SYM, KC_SPC),  RALT_T(KC_ESC)
+  ),
+
+    [_NUM] = LAYOUT_split_3x6_3(
+  //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
+      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_PAST,      KC_7,       KC_8,       KC_9,       KC_NO,      XXXXXXX,
+  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
+      XXXXXXX,      KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,              KC_PSLS,      KC_4,       KC_5,       KC_6,       KC_PENT,      XXXXXXX,
+  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
+      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_1,       KC_2,       KC_3,       KC_SLSH,      XXXXXXX,
+  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
+                                            XXXXXXX,    XXXXXXX, MO(_NAV),              KC_BSPC,    KC_0,       XXXXXXX
+  ),
+
     [_SYM] = LAYOUT_split_3x6_3(
   //.--------------------------------------------------------------------.              .-------------------------------------------------------------------.
       XXXXXXX,      KC_NO,      KC_LPRN,    KC_RPRN,    S(KC_QUOT), KC_QUOT,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
@@ -48,26 +70,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //.---------------------------------------------------   -_  ----------|              |-------------------------------------------------------------------|
                                             XXXXXXX,    KC_ESC,     KC_BSPC,            XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
+
     [_NAV] = LAYOUT_split_3x6_3(
   //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
       DF(_CLMK),      KC_NO,     KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_BRIU,    XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
       DF(_BASE),      KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,              KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   KC_MUTE,    XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_BRID,    XXXXXXX,
+      TG(_NUM),      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_BRID,    XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
                                             XXXXXXX,    XXXXXXX,     XXXXXXX,              KC_MPLY,    KC_DEL,     XXXXXXX
-  ),
-
-    [_NUM] = LAYOUT_split_3x6_3(
-  //.-------------------------------------------------------------------.               .-------------------------------------------------------------------.
-      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_7,       KC_8,       KC_9,       KC_NO,      XXXXXXX,
-  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,              KC_NO,      KC_4,       KC_5,       KC_6,       KC_NO,      XXXXXXX,
-  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-      XXXXXXX,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,      KC_1,       KC_2,       KC_3,       KC_NO,      XXXXXXX,
-  //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
-                                            XXXXXXX,    XXXXXXX,      XXXXXXX,              KC_BSPC,    KC_0,       XXXXXXX
   ),
 
     [_FN] = LAYOUT_split_3x6_3(
@@ -79,16 +91,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX,      KC_F10,     KC_F1,      KC_F2,      KC_F3,      KC_NO,              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,
   //|-------------------------------------------------------------------|               |-------------------------------------------------------------------|
                                             XXXXXXX,    KC_NO,      KC_NO,              XXXXXXX,     XXXXXXX,       XXXXXXX
-  ),
-
-    [_CLMK] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                             ,-----------------------------------------------------.
-      XXXXXXX,         KC_Q,         KC_W,       KC_F,           KC_P,    KC_B,          KC_J,         KC_L,         KC_U,         KC_Y,           KC_SCLN,  KC_MINS,
-  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),    KC_G,          KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,         KC_Z,         KC_X,       KC_C,           KC_D,    KC_B,          KC_K,         KC_H,      KC_COMM,       KC_DOT,         KC_SLSH,  KC_ESC,
-  //|--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-                            LGUI_T(KC_DEL), LT(_NUM, KC_BSPC), LT(_NAV, KC_ENT),           LT(_FN, KC_TAB), LT(_SYM, KC_SPC),  RALT_T(KC_ESC)
   ),
 };;
